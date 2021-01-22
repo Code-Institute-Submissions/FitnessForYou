dbdffbb9db112b936394a044cba702a556b44446
QuinnCunningham <quinncunningham10@outlook.com>
2021-01-22 21:54:50 +0000

    initalCommit

---
+++ cloudhomelab/urls.py
@@ -0,0 +1,19 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from membership import views
+
+urlpatterns = [
+    path('membership/', include('membership.urls')),
+    path('', views.home, name='home'),
+    path('admin/', admin.site.urls),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('auth/signup', views.SignUp.as_view(), name='signup'),
+    path('auth/settings', views.settings, name='settings'),
+    path('join', views.join, name='join'),
+    path('checkout', views.checkout, name='checkout'),
+    path('success', views.success, name='success'),
+    path('cancel', views.cancel, name='cancel'),
+
+
+]
